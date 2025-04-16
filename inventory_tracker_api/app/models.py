d419369fe7ca66465cfa36f85e94e75deff3a66d
Emmanuel Odenyire <eodenyire@gmail.com>
2025-04-16 22:24:41 +0300

    Update models.py

--- inventory_tracker_api/app/models.py
+++ inventory_tracker_api/app/models.py
@@ -1,1 +1,23 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from .database import Base
+
+class Supplier(Base):
+    __tablename__ = "suppliers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), unique=True, index=True)
+    contact = Column(String(100))
+    products = relationship("Product", back_populates="supplier")
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), index=True)
+    description = Column(String(255))
+    price = Column(Integer)
+    supplier_id = Column(Integer, ForeignKey("suppliers.id"))
+
+    supplier = relationship("Supplier", back_populates="products")
 
