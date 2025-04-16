ef71a01fa1a0c3469538ce17655c932be02f0a31
Emmanuel Odenyire <eodenyire@gmail.com>
2025-04-16 22:24:20 +0300

    Update schemas.py

--- inventory_tracker_api/app/schemas.py
+++ inventory_tracker_api/app/schemas.py
@@ -1,1 +1,30 @@
+from pydantic import BaseModel
+
+class SupplierBase(BaseModel):
+    name: str
+    contact: str
+
+class SupplierCreate(SupplierBase):
+    pass
+
+class Supplier(SupplierBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class ProductBase(BaseModel):
+    name: str
+    description: str
+    price: int
+    supplier_id: int
+
+class ProductCreate(ProductBase):
+    pass
+
+class Product(ProductBase):
+    id: int
+
+    class Config:
+        orm_mode = True
 
